556843e0d88e0b21e92cb37e2ece3570e5521e48
Andrew Young <andrew@vaelen.org>
2019-11-09 04:01:25 +0900

    Cleaned up tests

--- collections/linkedlist_test.go
+++ collections/linkedlist_test.go
@@ -28,98 +28,94 @@ import (
 	"testing"
 )
 
-func TestLinkedList(t *testing.T) {
-	t.Run("AddFirst", func(t *testing.T) {
-		list := &LinkedList{}
-		for i, v := range []int{1, 2, 3} {
-			if list.Size() != i {
-				t.Fatalf("Expected size %v but got %v\n", i, list.Size())
-			}
-			list.AddFirst(v)
+func TestLinkedListAddFirst(t *testing.T) {
+	list := &LinkedList{}
+	for i, v := range []int{1, 2, 3} {
+		if list.Size() != i {
+			t.Fatalf("Expected size %v but got %v\n", i, list.Size())
 		}
-		if list.Size() != 3 {
-			t.Fatalf("Expected size %v but got %v\n", 3, list.Size())
-		}
+		list.AddFirst(v)
+	}
+	if list.Size() != 3 {
+		t.Fatalf("Expected size %v but got %v\n", 3, list.Size())
+	}
 
-		e := [3]int{3, 2, 1}
-		v := [3]int{}
-		for i := range v {
-			v[i] = list.RemoveFirst().(int)
-		}
-		if e != v {
-			t.Fatalf("Expected contents to be %v but got %v\n", e, v)
-		}
-	})
+	e := [3]int{3, 2, 1}
+	v := [3]int{}
+	for i := range v {
+		v[i] = list.RemoveFirst().(int)
+	}
+	if e != v {
+		t.Fatalf("Expected contents to be %v but got %v\n", e, v)
+	}
+}
 
-	t.Run("AddLast", func(t *testing.T) {
-		list := &LinkedList{}
-		for i, v := range []int{1, 2, 3} {
-			if list.Size() != i {
-				t.Fatalf("Expected size %v but got %v\n", i, list.Size())
-			}
-			list.AddLast(v)
+func TestLinkedListAddLast(t *testing.T) {
+	list := &LinkedList{}
+	for i, v := range []int{1, 2, 3} {
+		if list.Size() != i {
+			t.Fatalf("Expected size %v but got %v\n", i, list.Size())
 		}
-		if list.Size() != 3 {
-			t.Fatalf("Expected size %v but got %v\n", 3, list.Size())
-		}
+		list.AddLast(v)
+	}
+	if list.Size() != 3 {
+		t.Fatalf("Expected size %v but got %v\n", 3, list.Size())
+	}
 
-		e := [3]int{1, 2, 3}
-		v := [3]int{}
-		for i := range v {
-			v[i] = list.RemoveFirst().(int)
-		}
-		if e != v {
-			t.Fatalf("Expected contents to be %v but got %v\n", e, v)
-		}
-	})
+	e := [3]int{1, 2, 3}
+	v := [3]int{}
+	for i := range v {
+		v[i] = list.RemoveFirst().(int)
+	}
+	if e != v {
+		t.Fatalf("Expected contents to be %v but got %v\n", e, v)
+	}
+}
 
-	t.Run("RemoveFirst", func(t *testing.T) {
-		list := &LinkedList{}
-		for _, v := range []int{1, 2, 3} {
-			list.AddLast(v)
+func TestLinkedListRemoveFirst(t *testing.T) {
+	list := &LinkedList{}
+	for _, v := range []int{1, 2, 3} {
+		list.AddLast(v)
+	}
+	for i, e := range []int{1, 2, 3} {
+		if list.Size() != 3-i {
+			t.Fatalf("Expected size %v but got %v\n", i, list.Size())
 		}
-		for i, e := range []int{1, 2, 3} {
-			if list.Size() != 3-i {
-				t.Fatalf("Expected size %v but got %v\n", i, list.Size())
-			}
-			v := list.RemoveFirst().(int)
-			if v != e {
-				t.Fatalf("Expected %v but got %v\n", e, v)
-			}
+		v := list.RemoveFirst().(int)
+		if v != e {
+			t.Fatalf("Expected %v but got %v\n", e, v)
 		}
-	})
+	}
+}
 
-	t.Run("RemoveLast", func(t *testing.T) {
-		list := &LinkedList{}
-		for _, v := range []int{1, 2, 3} {
-			list.AddLast(v)
+func TestLinkedListRemoveLast(t *testing.T) {
+	list := &LinkedList{}
+	for _, v := range []int{1, 2, 3} {
+		list.AddLast(v)
+	}
+	for i, e := range []int{3, 2, 1} {
+		if list.Size() != 3-i {
+			t.Fatalf("Expected size %v but got %v\n", i, list.Size())
 		}
-		for i, e := range []int{3, 2, 1} {
-			if list.Size() != 3-i {
-				t.Fatalf("Expected size %v but got %v\n", i, list.Size())
-			}
-			v := list.RemoveLast().(int)
-			if v != e {
-				t.Fatalf("Expected %v but got %v\n", e, v)
-			}
+		v := list.RemoveLast().(int)
+		if v != e {
+			t.Fatalf("Expected %v but got %v\n", e, v)
 		}
-	})
+	}
+}
 
-	t.Run("Iterators", func(t *testing.T) {
+func TestLinkedListIterators(t *testing.T) {
+	data := []int{1, 2, 3}
+	reverseData := []int{3, 2, 1}
 
-		data := []int{1, 2, 3}
-		reverseData := []int{3, 2, 1}
+	list := &LinkedList{}
+	for _, v := range []int{1, 2, 3} {
+		list.AddLast(v)
+	}
 
-		list := &LinkedList{}
-		for _, v := range []int{1, 2, 3} {
-			list.AddLast(v)
-		}
-
-		t.Run("Iterator", testIterator(list, list.Iterator(), data))
-		t.Run("ForwardIterator", testListIterator(list, list.ForwardIterator(), data, reverseData))
-		t.Run("ReverseIterator", testListIterator(list, list.ReverseIterator(), reverseData, data))
-	})
-
+	t.Run("Iterator", testIterator(list, list.Iterator(), data))
+	t.Run("ForwardIterator", testListIterator(list, list.ForwardIterator(), data, reverseData))
+	t.Run("ReverseIterator", testListIterator(list, list.ReverseIterator(), reverseData, data))
 }
 
 func TestQueue(t *testing.T) {
